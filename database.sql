c77d1a613dae2084ee38450777ccf4cde96c827b
Kalib Hackin <naguihw@gmail.com>
2020-12-18 00:10:28 -0500

    adding postgresql database

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE cars_database;
+
+--\c into cars_database
+
+CREATE TABLE cars(
+    cars_id SERIAL PRIMARY KEY,
+    model VARCHAR(255),
+    carType VARCHAR(255),
+    price INT,
+    year INT,
+    horsePower INT,
+    seats INT,
+    doors INT,
+    transmission VARCHAR(255),
+    quantity INT,
+    imagesLink TEXT[]
+);
+
+INSERT INTO cars (model, carType, price, year, horsePower, seats, doors, transmission, quantity, imagesLink)
+VALUES ('Toyota Highlander', 'SUV', 75, 2020, 295, 7, 4, 'Auto', 10, '{"https://s3.amazonaws.com/images.gearjunkie.com/uploads/2019/04/2020_Highlander_01_2FADC9D6C28B50D22F2FFF4339FCB81C59DC1810.jpg"}');
--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const {pool} = require('./config')
 
-const app = express();
+const app = express()
 
-app.use(cors({ origin: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello world');
-});
+const getCars = (request, response) => {
+  pool.query('SELECT * FROM cars', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
-const PORT = process.env.PORT || 8000;
+const addCar = (request, response) => {
+  const {model, carType, price, year, horsePower, seats, doors, transmission, quantity, imagesLink} = request.body
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-});
+  pool.query(
+    'INSERT INTO cars (model, carType, price, year, horsePower, seats, doors, transmission, quantity, imagesLink) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+    [model, carType, price, year, horsePower, seats, doors, transmission, quantity, imagesLink],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({status: 'success', message: 'Car added.'})
+    },
+  )
+}
+
+app
+  .route('/cars')
+  // GET endpoint
+  .get(getCars)
+  // POST endpoint
+  .post(addCar)
+
+// Start server
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server listening`)
+})
